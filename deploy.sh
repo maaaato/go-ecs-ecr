f03f694197ba2ed267ec392f1c6ee8cac9610dda
Masayuki.Nakano <masayuki.nakano@kyash.co>
2018-01-18 17:47:24 +0900

    modify

--- deploy.sh
+++ deploy.sh
@@ -60,7 +60,7 @@ make_task_def(){
 
 push_ecr_image(){
 	eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+	#docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
 }
 
 register_definition() {
@@ -76,4 +76,4 @@ register_definition() {
 
 configure_aws_cli
 push_ecr_image
-deploy_cluster
+#deploy_cluster
